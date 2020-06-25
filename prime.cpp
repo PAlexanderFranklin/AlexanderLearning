55500beb98aef5070cebc7489bcb8ceb25e9aa3c
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-24 18:08:23 -0700

    Simplify return statement

--- prime.cpp
+++ prime.cpp
@@ -4,7 +4,7 @@ bool isPrime(int x) {
 	if (x < 1)
 		return false;
 	if (x % 2 == 0)
-		return (x == 2);
+		return x == 2;
 	for (int i{ 3 }; i <= sqrt(x); i = i + 2) {
 		if (x % i == 0)
 			return false;
