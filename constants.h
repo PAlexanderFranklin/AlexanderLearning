717dd5f2c0b206fa705823caf15aa0604b4e9a00
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-29 18:30:07 -0700

    Inline

--- constants.h
+++ constants.h
@@ -1,7 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 namespace alexConst {
-	constexpr double gravity{ 9.8 };
-	constexpr double timeStep{ 1.0 / 1000.0 };
+	inline constexpr double gravity{ 9.8 };
+	inline constexpr double timeStep{ 1.0 / 1000.0 };
 }
 #endif
